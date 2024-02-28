0e1021117b13c4617072d054ed4a8131bcfd5c5f
Kolya Krafeld <k.krafeld@gmail.com>
2024-02-28 14:07:43 +0100

    Coordiantion state beginning

--- src/main/MessageHandler.java
+++ src/main/MessageHandler.java
@@ -14,6 +14,25 @@ public class MessageHandler {
     }
 
     /**
+     * Send a message to a specific node.
+     */
+    public void send(String message, int port, ConcurrentHashMap<Integer, Socket> nodeOutputSockets) {
+        try {
+            Socket socket;
+            if (nodeOutputSockets.containsKey(port)) {
+                socket = nodeOutputSockets.get(port);
+            } else {
+                socket = new Socket("127.0.0.1", port);
+                nodeOutputSockets.put(port, socket);
+            }
+            DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+            dout.writeUTF(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * Broadcast messages to all nodes in the network.
      */
     public void broadcast(String message, List<Integer> nodesPorts, ConcurrentHashMap<Integer, Socket> nodeOutputSockets) {
